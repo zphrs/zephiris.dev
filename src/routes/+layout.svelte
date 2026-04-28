99d37cc7a3a2d63c11072c943997a1c2b6f91083
zphrs <z@zephiris.dev>
2026-04-28 00:42:34 -0700

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import favicon from "$lib/assets/favicon.svg";
+    import "$lib/styles/reset.css";
+    import "$lib/styles/base.css";
+
+    // Supports weights 200-800
+    import "@fontsource-variable/atkinson-hyperlegible-next/wght.css";
+
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
